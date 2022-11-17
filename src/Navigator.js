3a0069f3bd622c46e93d8c129301ab913e352e91
Neel <neelnarayanshetty@gmail.com>
2022-11-18 00:47:19 +0530

    bottom tab bar

--- src/Navigator.js
+++ src/Navigator.js
@@ -3,18 +3,84 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
-import TabBar from "./components/sharedComponents/TabBar";
+import AddTaskScreen from "./screens/AddTaskScreen";
+import ReportScreen from "./screens/ReportScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Tabs = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function StackScreens() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="AddTask"
+        component={AddTaskScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 const Navigator = () => {
   return (
     <NavigationContainer>
-      <Tabs.Navigator >
-        <Tabs.Screen 
+      <Tabs.Navigator screenOptions={{ tabBarShowLabel: false }}>
+        <Tabs.Screen
           name="HomeScreen"
           component={HomeScreen}
-          options={{ headerShown: false }}
+          options={{
+            headerShown: false,
+            tabBarStyle: {
+              position: "absolute",
+              // bottom:25,
+              height: 90,
+              borderTopEndRadius: 35,
+              borderTopStartRadius: 35,
+            },
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={focused ? "time" : "time-outline"}
+                size={24}
+                color="black"
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="AddScreen"
+          component={StackScreens}
+          options={{
+            headerShown: false,
+            tabBarStyle: {
+              display:'none'
+            },
+            tabBarIcon: ({ focused }) => (
+              <Ionicons name="add-circle" size={49} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="ReportScreen"
+          component={ReportScreen}
+          options={{
+            headerShown: false,
+            tabBarStyle: {
+              position: "absolute",
+              // bottom:25,
+              height: 90,
+              borderTopEndRadius: 35,
+              borderTopStartRadius: 35,
+            },
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={focused ? "pie-chart" : "pie-chart-outline"}
+                size={24}
+                color="black"
+              />
+            ),
+          }}
         />
       </Tabs.Navigator>
     </NavigationContainer>
---
+++ src/screens/AddTaskScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AddTaskScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>AddTaskScreennnnnnnnnnnnnnnn</Text>
+    </View>
+  );
+};
+
+export default AddTaskScreen;
+
+const styles = StyleSheet.create({});
