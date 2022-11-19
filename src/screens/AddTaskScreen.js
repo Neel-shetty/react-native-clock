412d44950d3319edbb6870ff41326cbde49aa13c
Neel <neelnarayanshetty@gmail.com>
2022-11-20 02:28:35 +0530

    update

--- src/screens/AddTaskScreen.js
+++ src/screens/AddTaskScreen.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Header from "../components/sharedComponents/Header";
 
 const AddTaskScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>AddTaskScreennnnnnnnnnnnnnnn</Text>
+      <Header />
+      <CustomInput />
     </View>
   );
 };
